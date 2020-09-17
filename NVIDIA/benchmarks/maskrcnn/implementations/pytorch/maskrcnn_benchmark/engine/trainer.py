318870835c440fa9ceb46ea124c66fc8baf05138
Gautam Kumar <gauta@amazon.com>
2020-09-17 11:37:59 -0700

    adding nvtx profiler

--- NVIDIA/benchmarks/maskrcnn/implementations/pytorch/maskrcnn_benchmark/engine/trainer.py
+++ NVIDIA/benchmarks/maskrcnn/implementations/pytorch/maskrcnn_benchmark/engine/trainer.py
@@ -11,6 +11,8 @@ from maskrcnn_benchmark.utils.comm import get_world_size, is_main_process, synch
 from maskrcnn_benchmark.utils.metric_logger import MetricLogger
 
 from apex import amp
+import torch.cuda.profiler as profiler
+import pyprof
 
 def reduce_loss_dict(loss_dict):
     """
@@ -56,6 +58,7 @@ def do_train(
     per_iter_start_callback_fn=None,
     per_iter_end_callback_fn=None,
 ):
+    pyprof.init()
     logger = logging.getLogger("maskrcnn_benchmark.trainer")
     logger.info("Start training")
     meters = MetricLogger(delimiter="  ")
@@ -94,78 +97,80 @@ def do_train(
     
     synchronize()
     optimizer.zero_grad()
-    for iteration, (images, targets) in enumerate(prefetcher(iter(data_loader)), start_iter):
-        if per_iter_start_callback_fn is not None:
-            per_iter_start_callback_fn(iteration=iteration)
+    # PyTorch NVTX context manager
+    with torch.autograd.profiler.emit_nvtx():
+       for iteration, (images, targets) in enumerate(prefetcher(iter(data_loader)), start_iter):
+           if per_iter_start_callback_fn is not None:
+               per_iter_start_callback_fn(iteration=iteration)
 
-        data_time = time.time() - end
-        iteration = iteration + 1
-        arguments["iteration"] = iteration
+           data_time = time.time() - end
+           iteration = iteration + 1
+           arguments["iteration"] = iteration
 
 
-        images = images.to(device)
-        targets = [target.to(device) for target in targets]
+           images = images.to(device)
+           targets = [target.to(device) for target in targets]
 
-        loss_dict = model(images, targets)
+           loss_dict = model(images, targets)
 
-        losses = sum(loss for loss in loss_dict.values())
+           losses = sum(loss for loss in loss_dict.values())
 
-        # reduce losses over all GPUs for logging purposes
-        if not disable_allreduce_for_logging:
-            loss_dict_reduced = reduce_loss_dict(loss_dict)
-            losses_reduced = sum(loss for loss in loss_dict_reduced.values())
-            meters.update(loss=losses_reduced, **loss_dict_reduced)
-        else:
-            meters.update(loss=losses, **loss_dict)
+           # reduce losses over all GPUs for logging purposes
+           if not disable_allreduce_for_logging:
+               loss_dict_reduced = reduce_loss_dict(loss_dict)
+               losses_reduced = sum(loss for loss in loss_dict_reduced.values())
+               meters.update(loss=losses_reduced, **loss_dict_reduced)
+           else:
+               meters.update(loss=losses, **loss_dict)
 
-        # optimizer.zero_grad()
-        # Note: If mixed precision is not used, this ends up doing nothing
-        # Otherwise apply loss scaling for mixed-precision recipe
-        # with optimizer.scale_loss(losses) as scaled_losses:
-        optimizer.backward(losses)
-        optimizer.step()
-        # set_grads_to_none(model)
-        optimizer.zero_grad()
-        scheduler.step()
+           # optimizer.zero_grad()
+           # Note: If mixed precision is not used, this ends up doing nothing
+           # Otherwise apply loss scaling for mixed-precision recipe
+           # with optimizer.scale_loss(losses) as scaled_losses:
+           optimizer.backward(losses)
+           optimizer.step()
+           # set_grads_to_none(model)
+           optimizer.zero_grad()
+           scheduler.step()
 
-        batch_time = time.time() - end
-        end = time.time()
-        meters.update(time=batch_time, data=data_time)
+           batch_time = time.time() - end
+           end = time.time()
+           meters.update(time=batch_time, data=data_time)
 
-        eta_seconds = meters.time.global_avg * (max_iter - iteration)
-        eta_string = str(datetime.timedelta(seconds=int(eta_seconds)))
+           eta_seconds = meters.time.global_avg * (max_iter - iteration)
+           eta_string = str(datetime.timedelta(seconds=int(eta_seconds)))
 
-        if iteration % 20 == 0 or iteration == max_iter:
-            logger.info(
-                meters.delimiter.join(
-                    [
-                        "eta: {eta}",
-                        "iter: {iter}",
-                        "{meters}",
-                        "lr: {lr:.6f}",
-                        "max mem: {memory:.0f}",
-                    ]
-                ).format(
-                    eta=eta_string,
-                    iter=iteration,
-                    meters=str(meters),
-                    lr=optimizer.param_groups[0]["lr"],
-                    memory=torch.cuda.max_memory_allocated() / 1024.0 / 1024.0,
-                )
-            )
-        if iteration % checkpoint_period == 0 and arguments["save_checkpoints"]:
-            checkpointer.save("model_{:07d}".format(iteration), **arguments)
-        if iteration == max_iter and arguments["save_checkpoints"]:
-            checkpointer.save("model_final", **arguments)
+           if iteration % 20 == 0 or iteration == max_iter:
+               logger.info(
+                   meters.delimiter.join(
+                       [
+                           "eta: {eta}",
+                           "iter: {iter}",
+                           "{meters}",
+                           "lr: {lr:.6f}",
+                           "max mem: {memory:.0f}",
+                       ]
+                   ).format(
+                       eta=eta_string,
+                       iter=iteration,
+                       meters=str(meters),
+                       lr=optimizer.param_groups[0]["lr"],
+                       memory=torch.cuda.max_memory_allocated() / 1024.0 / 1024.0,
+                   )
+               )
+           if iteration % checkpoint_period == 0 and arguments["save_checkpoints"]:
+               checkpointer.save("model_{:07d}".format(iteration), **arguments)
+           if iteration == max_iter and arguments["save_checkpoints"]:
+               checkpointer.save("model_final", **arguments)
 
-        # per-epoch work (testing)
-        if per_iter_end_callback_fn is not None:
-            # Note: iteration has been incremented previously for
-            # human-readable checkpoint names (i.e. 60000 instead of 59999)
-            # so need to adjust again here
-            early_exit = per_iter_end_callback_fn(iteration=iteration-1)
-            if early_exit:
-                break
+           # per-epoch work (testing)
+           if per_iter_end_callback_fn is not None:
+               # Note: iteration has been incremented previously for
+               # human-readable checkpoint names (i.e. 60000 instead of 59999)
+               # so need to adjust again here
+               early_exit = per_iter_end_callback_fn(iteration=iteration-1)
+               if early_exit:
+                   break
 
     total_training_time = time.time() - start_training_time
     total_time_str = str(datetime.timedelta(seconds=total_training_time))
